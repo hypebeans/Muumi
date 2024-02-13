c632e50be217e59a361b11ab5403347391b5fe12
hypebeans <24372255+hypebeans@users.noreply.github.com>
2024-02-13 13:39:52 +0900

    Initial Commit

---
+++ Muumi/MuumiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuumiApp.swift
+//  Muumi
+//
+//  Created by Jonathan Budiman on 2024/02/13.
+//
+
+import SwiftUI
+
+@main
+struct MuumiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
